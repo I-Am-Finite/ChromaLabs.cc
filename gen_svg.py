b9c3b3b9d6e0a418772354896af79f5b1097d118
I-Am-Finite <I-Am-Finite@users.noreply.github.com>
2026-08-28 17:35:37 -0500

    fix: Expand SVG viewBox width to 550 to physically prevent horizontal typography cutoff

--- gen_svg.py
+++ gen_svg.py
@@ -3,9 +3,9 @@ import base64
 with open(r'C:\Dev\ChromaLabs.cc\assets\logo_glyph.png', 'rb') as img_file:
     b64_string = base64.b64encode(img_file.read()).decode('utf-8')
 
-# The PNG is 542x563
-# We will give it a massive viewBox to ensure zero cutoff: 500x120
-svg_horizontal = f'''<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 120" fill="none">
+# The text "CHROMA LABS" at font-size 48 takes up more than 380 units of width.
+# With x=120, it extends past x=500. We set viewBox to 600 to be perfectly safe.
+svg_horizontal = f'''<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 550 120" fill="none">
   <image href="data:image/png;base64,{b64_string}" x="0" y="5" width="105" height="110" />
   <text x="120" y="80" font-family="Inter, sans-serif" font-weight="900" font-size="48" fill="#DFBC61" letter-spacing="1.5">CHROMA LABS</text>
 </svg>'''
