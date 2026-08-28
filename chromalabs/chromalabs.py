ecd536b4b06c65fe3b4658b757c00175fb9c3fe1
I-Am-Finite <I-Am-Finite@users.noreply.github.com>
2026-08-27 23:14:08 -0500

    fix(build): Remove invalid overlay_component prop from rx.App

--- chromalabs/chromalabs.py
+++ chromalabs/chromalabs.py
@@ -11,7 +11,6 @@ app = rx.App(
         ".reflex-watermark": {"display": "none !important"},
         "#reflex-connection-error": {"display": "none !important"},
     },
-    overlay_component=rx.fragment(),
 )
 app.add_page(index, route="/", title="Chroma Labs", image="/logo.svg")
 app.add_page(technology, route="/technology", title="Technology | Chroma Labs")
